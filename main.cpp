a983f052dedf8a37b2eb2916abc53d2ed0909835
derselbst <tom.mbrt@googlemail.com>
2014-12-02 18:15:13 +0100

    update demo prog

--- main.cpp
+++ main.cpp
@@ -13,7 +13,22 @@ int main()
     exit(-1);
   }
   
-  sf.add_new_sample(sample, SampleType::SIGNED_16, "someSample", 2000, 2288/sizeof(int16_t), true, 1000, 1100, 10, 55, 31183);
+  sf.add_new_sample(sample, SampleType::SIGNED_16, "someSample", 0x2c, 2288/sizeof(int16_t), true, 0x11, 0x042e, 0x3c, 1, 31183);
+  sf.add_new_instrument("someinstr");
+  
+  sf.add_new_inst_bag();
+  sf.add_new_inst_generator(SFGenerator::sampleModes, 1); //enable looping
+  sf.add_new_inst_generator(SFGenerator::sampleID, 0); // link someinstr to sample 0
+  sf.add_new_inst_generator(SFGenerator::keyRange, 0,80); // specify key range
+  
+  sf.add_new_inst_bag();
+  sf.add_new_inst_generator(SFGenerator::sampleModes, 1); //enable looping
+  sf.add_new_inst_generator(SFGenerator::sampleID, 0); // link someinstr to sample 0
+  sf.add_new_inst_generator(SFGenerator::keyRange, 81, 127); //specify key range
+  
+  sf.add_new_preset("somepresset", 10, 0);
+  sf.add_new_preset_bag();
+  sf.add_new_preset_generator(SFGenerator::instrument, 0);
   
   FILE* sfout = fopen("./sftest.sf2", "wb");
   if (!sfout)
