3aec060306d9f726b3e8b1669a968c8faa56017a
derselbst <tom.mbrt@googlemail.com>
2016-09-21 10:03:08 +0200

    format

--- sf2.hpp
+++ sf2.hpp
@@ -15,65 +15,65 @@ class sfInst;
 // This is our own type, not part of the specification
 typedef enum
 {
-	UNSIGNED_8,
-	SIGNED_8,
-	SIGNED_16,
-	GAMEBOY_CH3,
-	BDPCM
+    UNSIGNED_8,
+    SIGNED_8,
+    SIGNED_16,
+    GAMEBOY_CH3,
+    BDPCM
 }
 SampleType;
 
 class SF2
 {
-	uint32_t size;
+    uint32_t size;
 // Instrument and samples objects
 //	SF2Instr& instruments;
 //	gba_samples& samples;
 
-	//All 3 chunks of the SF2 file
-	InfoListChunk *const infolist_chunk;
-	SdtaListChunk *const sdtalist_chunk;
-	HydraChunk *const pdtalist_chunk;
+    //All 3 chunks of the SF2 file
+    InfoListChunk *const infolist_chunk;
+    SdtaListChunk *const sdtalist_chunk;
+    HydraChunk *const pdtalist_chunk;
 
-	void add_terminals();
+    void add_terminals();
 
 // Forbid copy and affectation
-	SF2(SF2&);
-	SF2& operator=(SF2&);
+    SF2(SF2&);
+    SF2& operator=(SF2&);
 
 public:
-	//Target file, should be assigned to a valid opened FILE in "wb" mode by user before "write()" is called.
-	FILE *out;
-	unsigned int default_sample_rate;
+    //Target file, should be assigned to a valid opened FILE in "wb" mode by user before "write()" is called.
+    FILE *out;
+    unsigned int default_sample_rate;
 
-	SF2(unsigned int sample_rate);
-	~SF2();
-	void write(FILE *outfile);
-	void add_new_preset(const char *name, int Patch, int Bank);
-	void add_new_instrument(const char *name);
-	void add_new_inst_bag();
-	void add_new_preset_bag();
-	void add_new_preset_modulator();
-	void add_new_preset_generator();
-	void add_new_preset_generator(SFGenerator operation, uint16_t value);
-	void add_new_preset_generator(SFGenerator operation, uint8_t lo, uint8_t hi);
-	void add_new_inst_modulator();
-	void add_new_inst_generator();
-	void add_new_inst_generator(SFGenerator operation, uint16_t value);
-	void add_new_inst_generator(SFGenerator operation, uint8_t lo, uint8_t hi);
-	void add_new_sample_header(const char *name, int start, int end, int start_loop, int end_loop, int sample_rate, int original_pitch, int pitch_correction);
+    SF2(unsigned int sample_rate);
+    ~SF2();
+    void write(FILE *outfile);
+    void add_new_preset(const char *name, int Patch, int Bank);
+    void add_new_instrument(const char *name);
+    void add_new_inst_bag();
+    void add_new_preset_bag();
+    void add_new_preset_modulator();
+    void add_new_preset_generator();
+    void add_new_preset_generator(SFGenerator operation, uint16_t value);
+    void add_new_preset_generator(SFGenerator operation, uint8_t lo, uint8_t hi);
+    void add_new_inst_modulator();
+    void add_new_inst_generator();
+    void add_new_inst_generator(SFGenerator operation, uint16_t value);
+    void add_new_inst_generator(SFGenerator operation, uint8_t lo, uint8_t hi);
+    void add_new_sample_header(const char *name, int start, int end, int start_loop, int end_loop, int sample_rate, int original_pitch, int pitch_correction);
 
-        void add_new_sample(FILE *file, SampleType type, const char *name, uint32_t pointer, uint32_t size, bool loop_flag,
-                                  uint32_t loop_start, uint32_t loop_end, uint32_t original_pitch, uint32_t pitch_correction, uint32_t sample_rate);
-        void add_new_sample(void *mem, SampleType type, const char *name, uint32_t pointer, uint32_t size, bool loop_flag,
-                                  uint32_t loop_start, uint32_t loop_end, uint32_t original_pitch, uint32_t pitch_correction, uint32_t sample_rate);
+    void add_new_sample(FILE *file, SampleType type, const char *name, uint32_t pointer, uint32_t size, bool loop_flag,
+                        uint32_t loop_start, uint32_t loop_end, uint32_t original_pitch, uint32_t pitch_correction, uint32_t sample_rate);
+    void add_new_sample(void *mem, SampleType type, const char *name, uint32_t pointer, uint32_t size, bool loop_flag,
+                        uint32_t loop_start, uint32_t loop_end, uint32_t original_pitch, uint32_t pitch_correction, uint32_t sample_rate);
 
-	uint16_t get_ibag_size();
-	uint16_t get_igen_size();
-	uint16_t get_imod_size();
-	uint16_t get_pbag_size();
-	uint16_t get_pgen_size();
-	uint16_t get_pmod_size();
+    uint16_t get_ibag_size();
+    uint16_t get_igen_size();
+    uint16_t get_imod_size();
+    uint16_t get_pbag_size();
+    uint16_t get_pgen_size();
+    uint16_t get_pmod_size();
 };
 
 #endif
